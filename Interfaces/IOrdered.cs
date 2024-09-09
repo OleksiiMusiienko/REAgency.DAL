be15792a02e1a00506fb1c8149c55d69a4c1e6fa
Oleksii <musienko1984@gmail.com>
2024-09-09 08:24:03 +0300

    Task160. Add interface IOrdered
    Added interface for working with the order

---
+++ Interfaces/IOrdered.cs
@@ -0,0 +1,18 @@
+﻿using REAgency.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REAgency.DAL.Interfaces
+{
+    public interface IOrdered
+    {
+        Task Create(Order clientOrder);
+        void Update(Order clientOrder);
+        Task Delete(int id);
+        Task<Order> Get(int id);
+        Task<IEnumerable<Order>> GetAll();
+    }
+}
