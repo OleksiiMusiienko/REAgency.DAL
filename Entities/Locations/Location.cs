28a50581f9c6acf191f0323ef52b620d92386aa5
OleksiiMusiienko <musienko1984@gmail.com>
2024-09-26 12:30:19 +0300

    Task202. Добавление поля DateTime
    Добавлено поле в класс Location

--- Entities/Locations/Location.cs
+++ Entities/Locations/Location.cs
@@ -11,6 +11,7 @@
         public int?  DistrictId { get; set; }
         public virtual Locality Locality { get; set; }
         public int? LocalityId { get; set; }
-        
+        public DateTime Date { get; set; }
+
     }
 }
